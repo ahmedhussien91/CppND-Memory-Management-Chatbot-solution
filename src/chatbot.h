1f91c82639bb978c95d11bbcef58ec426968b604
ahmed <a.hussein.91@gmail.com>
2021-12-06 09:45:18 +0200

    Task2: impelement rule of five for ChatBot.

--- src/chatbot.h
+++ src/chatbot.h
@@ -3,6 +3,7 @@
 
 #include <wx/bitmap.h>
 #include <string>
+#include <iostream>
 
 class GraphNode; // forward declaration
 class ChatLogic; // forward declaration
@@ -29,6 +30,47 @@ public:
 
     //// STUDENT CODE
     ////
+    // copy constructor
+    ChatBot(ChatBot & other) {
+        std::cout << "ChatBot copy constructor";
+        _image = new wxBitmap(*other._image);
+        _chatLogic = nullptr;
+        _rootNode = nullptr;
+    } 
+    // move constructor
+    ChatBot(ChatBot && other) {
+        std::cout << "ChatBot move constructor";
+        _image = other._image;
+        _currentNode = other._currentNode;
+        _rootNode = other._rootNode;
+        _chatLogic = other._chatLogic;
+        other._image = nullptr;
+        other._currentNode = nullptr;
+        other._rootNode = nullptr;
+        other._chatLogic = nullptr;
+    }
+    ChatBot & operator = ( ChatBot & other){
+        std::cout << "ChatBot copy assignment operator";
+        if (&other == this)
+            return *this;
+        delete _image;
+        _image = new wxBitmap(*other._image);
+    };
+    ChatBot& operator = (ChatBot && other){
+        std::cout << "ChatBot move assignment operator";
+        if (&other == this)
+            return *this;
+        delete _image;
+        _image = other._image;
+        _currentNode = other._currentNode;
+        _rootNode = other._rootNode;
+        _chatLogic = other._chatLogic;
+        other._image = nullptr;
+        other._currentNode = nullptr;
+        other._rootNode = nullptr;
+        other._chatLogic = nullptr;
+    };
+
 
     ////
     //// EOF STUDENT CODE
